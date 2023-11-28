c0667882ad52c65cc87ff9003e72b87f677da5c6
meghla khatun <meghlaparvin45@gmail.com>
2023-11-28 19:31:06 +0600

    added a details page

--- src/Pages/AllArticles/Details.jsx
+++ src/Pages/AllArticles/Details.jsx
@@ -1,14 +1,31 @@
-import { useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 
 const Details = () => {
 
-    const details=useLoaderData();
-    console.log(details)
+    const details = useLoaderData();
+    const { title, name, tag, description, image } = details;
 
     return (
         <div>
-           
+            <Navbar></Navbar>
+            <div className="hero  bg-fixed" style={{ backgroundImage: 'url(https://i.ibb.co/WcPfPzK/banner-1.jpg)' }}>
+                <div className="max-w-3xl mx-auto py-10 rounded-lg px-10">
+                    <div className="card bg-base-100 shadow-2xl shadow-gray-500">
+                        <div className="w-full">
+                            <img className="w-full rounded-t-lg" src={image}
+                            />
+                        </div>
+                        <div className="card-body">
+                            <h2 className="text-[18px] md:text-xl font-semibold"><span className="font-bold">Title:</span> {title}</h2>
+                            <h2 className="text-[18px] md:text-xl font-semibold"><span className="font-bold">Publisher:</span>  {name}</h2>
+                            <h2 className="text-[18px] md:text-xl font-semibold"><span className="font-bold">Tag:</span>  {tag}</h2>
+                            <p className="font-normal text-[15px]">{description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
